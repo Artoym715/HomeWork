eeed183051dfc7b26ce058a3feb8754dcdb1dbf8
Dmitry Bystrov <98824571+Artoym715@users.noreply.github.com>
2023-06-02 16:50:17 +0600

    add info

--- about.php
+++ about.php
@@ -5,12 +5,59 @@ $prof = "Программист";
 $city = "Казахстан , Алматы";
 $email = "b@gmail.com";
 $phone = 87778866778;
+$age = 2023 - 1993;
+$days = (2023 - 1993) * 365;
 $langs = [
-  'rus' => 'Русский',
-  'eng' => 'Английский',
-  'kaz' => 'Казахский'
+  [
+    'name' => 'Русский',
+    'value' => 100
+  ],
+  [
+    'name' => 'Английский',
+    'value' => 55
+  ],
+  [
+    'name' => 'Казахский',
+    'value' => 25
+  ]
+];
+$sckills = [
+  [
+    'name' => "Фотошоп",
+    'value' => 90
+  ],
+  [
+    'name' => "Программирование",
+    'value' => 80
+  ],
+  [
+    'name' => "Фронтент",
+    'value' => 75
+  ],
+  [
+    'name' => "Веб-дизайн",
+    'value' => 35
+  ],
+
 ];
 
+$work_exper = [
+  0 => [
+    'work_name' => 'Веб-программист',
+    'work_date' => 'март 2015 - ',
+    'work_text' => 'Работаю сейчас. Lorem ipsum dolor sit amet. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste.'
+  ],
+  1 => [
+    'work_name' => 'Веб-программист',
+    'work_date' => 'Март 2012 - декабрь 2014',
+    'work_text' => 'Моя вторая работа. Lorem ipsum dolor sit amet. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste.'
+  ],
+  2 => [
+    'work_name' => 'Веб-программист',
+    'work_date' => 'Июнь 2010 - март 2012',
+    'work_text' => 'Мой первый опыт. Lorem ipsum dolor sit amet. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste.'
+  ]
+];
 
 ?>
 
@@ -65,43 +112,26 @@ $langs = [
             <p><i class="fa fa-home fa-fw w3-margin-right w3-large w3-text-teal"></i><?= $city ?></p>
             <p><i class="fa fa-envelope fa-fw w3-margin-right w3-large w3-text-teal"></i><?= $email ?></p>
             <p><i class="fa fa-phone fa-fw w3-margin-right w3-large w3-text-teal"></i><?= $phone ?></p>
+            <p><i class="fa fa-hourglass-half fa-fw w3-margin-right w3-large w3-text-teal"></i><?php echo $age ?> лет (<?php echo $days ?> дней)</p>
             <hr>
 
             <p class="w3-large"><b><i class="fa fa-asterisk fa-fw w3-margin-right w3-text-teal"></i>Навыки</b></p>
-            <p>Adobe Photoshop</p>
-            <div class="w3-light-grey w3-round-xlarge w3-small">
-              <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:90%">90%</div>
-            </div>
-            <p>Photography</p>
-            <div class="w3-light-grey w3-round-xlarge w3-small">
-              <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:80%">
-                <div class="w3-center w3-text-white">80%</div>
+            <?php foreach ($sckills as $sckill) : ?>
+              <p><?= $sckill['name'] ?></p>
+              <div class="w3-light-grey w3-round-xlarge w3-small">
+                <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:<?= $sckill['value'] ?>%"><?= $sckill['value'] ?>%</div>
               </div>
-            </div>
-            <p>Illustrator</p>
-            <div class="w3-light-grey w3-round-xlarge w3-small">
-              <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:75%">75%</div>
-            </div>
-            <p>Media</p>
-            <div class="w3-light-grey w3-round-xlarge w3-small">
-              <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:50%">50%</div>
-            </div>
+            <?php endforeach; ?>
             <br>
 
             <p class="w3-large w3-text-theme"><b><i class="fa fa-globe fa-fw w3-margin-right w3-text-teal"></i>Языки</b>
             </p>
-            <p><?= $langs['rus'] ?></p>
-            <div class="w3-light-grey w3-round-xlarge">
-              <div class="w3-round-xlarge w3-teal" style="height:24px;width:100%"></div>
-            </div>
-            <p><?= $langs['eng'] ?></p>
-            <div class="w3-light-grey w3-round-xlarge">
-              <div class="w3-round-xlarge w3-teal" style="height:24px;width:55%"></div>
-            </div>
-            <p><?= $langs['kaz'] ?></p>
-            <div class="w3-light-grey w3-round-xlarge">
-              <div class="w3-round-xlarge w3-teal" style="height:24px;width:25%"></div>
-            </div>
+            <?php foreach ($langs as $lang) : ?>
+              <p><?= $lang['name'] ?></p>
+              <div class="w3-light-grey w3-round-xlarge">
+                <div class="w3-round-xlarge w3-teal" style="height:24px;width:<?= $lang['value'] ?>%"></div>
+              </div>
+            <?php endforeach; ?>
 
             <br>
           </div>
@@ -116,64 +146,60 @@ $langs = [
         <div class="w3-container w3-card w3-white w3-margin-bottom">
           <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-suitcase fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Опыт работы</h2>
           <div class="w3-container">
-            <h5 class="w3-opacity"><b>Front End Developer </b></h5>
-            <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Jan 2015 - <span class="w3-tag w3-teal w3-round">Current</span></h6>
-            <p>Lorem ipsum dolor sit amet. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit
-              sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste.</p>
-            <hr>
+            <?php foreach ($work_exper as $key => $work_exp) : ?>
+
+              <div class="w3-container">
+
+                <h5 class="w3-opacity"><b><?php echo $work_exp['work_name']; ?> </b></h5>
+                <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i><?php echo $work_exp['work_date']; ?>
+                  <?php if ($key == 0) : ?>
+                    <span class="w3-tag w3-teal w3-round">Current</span>
+                  <?php endif; ?>
+                </h6>
+                <p><?php echo $work_exp['work_text']; ?></p>
+                <hr>
+              </div>
+            <?php endforeach; ?>
           </div>
-          <div class="w3-container">
-            <h5 class="w3-opacity"><b>Web Developer / something.com</b></h5>
-            <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Mar 2012 - Dec 2014</h6>
-            <p>Consectetur adipisicing elit. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit
-              sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste.</p>
-            <hr>
+
+          <div class="w3-container w3-card w3-white">
+            <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-certificate fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Образование</h2>
+            <div class="w3-container">
+              <h5 class="w3-opacity"><b>gb.ru</b></h5>
+              <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Forever</h6>
+              <p>Web Development! All I need to know in one place</p>
+              <hr>
+            </div>
+            <div class="w3-container">
+              <h5 class="w3-opacity"><b>London Business School</b></h5>
+              <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2013 - 2015</h6>
+              <p>Master Degree</p>
+              <hr>
+            </div>
+            <div class="w3-container">
+              <h5 class="w3-opacity"><b>School of Coding</b></h5>
+              <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2010 - 2013</h6>
+              <p>Bachelor Degree</p><br>
+            </div>
           </div>
-          <div class="w3-container">
-            <h5 class="w3-opacity"><b>Graphic Designer / designsomething.com</b></h5>
-            <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Jun 2010 - Mar 2012</h6>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p><br>
-          </div>
+
+          <!-- End Right Column -->
         </div>
 
-        <div class="w3-container w3-card w3-white">
-          <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-certificate fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Образование</h2>
-          <div class="w3-container">
-            <h5 class="w3-opacity"><b>gb.ru</b></h5>
-            <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Forever</h6>
-            <p>Web Development! All I need to know in one place</p>
-            <hr>
-          </div>
-          <div class="w3-container">
-            <h5 class="w3-opacity"><b>London Business School</b></h5>
-            <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2013 - 2015</h6>
-            <p>Master Degree</p>
-            <hr>
-          </div>
-          <div class="w3-container">
-            <h5 class="w3-opacity"><b>School of Coding</b></h5>
-            <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2010 - 2013</h6>
-            <p>Bachelor Degree</p><br>
-          </div>
-        </div>
-
-        <!-- End Right Column -->
+        <!-- End Grid -->
       </div>
 
-      <!-- End Grid -->
+      <!-- End Page Container -->
     </div>
 
-    <!-- End Page Container -->
-  </div>
-
-  <!-- Footer -->
-  <footer class="w3-container w3-teal w3-center w3-margin-top">
-    <p>Find me on social media.</p>
-    <i class="fa fa-pinterest-p w3-hover-opacity"></i>
-    <i class="fa fa-twitter w3-hover-opacity"></i>
-    <i class="fa fa-linkedin w3-hover-opacity"></i>
-    <!-- End footer -->
-  </footer>
+    <!-- Footer -->
+    <footer class="w3-container w3-teal w3-center w3-margin-top">
+      <p>Find me on social media.</p>
+      <i class="fa fa-pinterest-p w3-hover-opacity"></i>
+      <i class="fa fa-twitter w3-hover-opacity"></i>
+      <i class="fa fa-linkedin w3-hover-opacity"></i>
+      <!-- End footer -->
+    </footer>
 
 </body>
 
